02a0bee6171ee3faa07545be376c36c42121e425
abhay0933 <abhay80912@gmail.com>
2024-01-10 22:22:51 +0530

    added some things

--- src/comps/PasswordGenerator.js
+++ src/comps/PasswordGenerator.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useRef } from "react";
 
 const PasswordGenerator = () => {
    
@@ -32,13 +32,23 @@ const PasswordGenerator = () => {
 
     }, [length, includeNumbers, includeSpecialChars]);
 
+    const copyRef= useRef(null);
+
+    const copypass = () => {
+        navigator.clipboard.writeText(pass)
+        .then(() => alert("Password copied to clipboard"))
+        .catch(err => console.error('Unable to copy password', err));
+    }
+
 
     return(
         <>
         <h1>Random Password Generator</h1>
-        <h3>{pass}</h3>
+        <h3 ref={copyRef}>{pass}</h3>
         <div className="btn">
         <button onClick={generatepass} className="btn">Generate Password</button> 
+       <button onClick={copypass}><svg xmlns="http://www.w3.org/2000/svg" height="13" width="12" viewBox="0 0 448 512"><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg></button>
+
         </div>
         <div className="length">
             <label>Password Length: 
@@ -55,7 +65,7 @@ const PasswordGenerator = () => {
                 <input type="checkbox" value={includeSpecialChars} onChange={(e)=>setIncludeSpeacialChars(e.target.value)}></input>
             </label>
         </div>
-
+       
          
         
         </>
